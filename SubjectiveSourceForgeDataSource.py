82e2e1e827ac6815ca7ac29597a29b72015ce1f0
PabloBorda <pablotomasborda@gmail.com>
2025-08-07 22:28:30 -0300

    Standardize get_icon method to load from icon.svg with fallback

--- SubjectiveSourceForgeDataSource.py
+++ SubjectiveSourceForgeDataSource.py
@@ -69,13 +69,15 @@ class SubjectiveSourceForgeDataSource(SubjectiveDataSource):
 
     # ------------------------------------------------------------------
     def get_icon(self):
-        """Return the SVG code for the SourceForge icon."""
-        return """
-<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-  <circle cx="512" cy="512" r="512" style="fill: rgb(255, 102, 0);" />
-  <path d="M548.2 525.8c0-83.5-29.7-121.5-45.5-135.9-3.1-2.6-7.9-.4-7.4 3.9 3.1 47.2-56.4 59-56.4 132.9v.4c0 45 34.1 81.7 76 81.7s76-36.7 76-81.7v-.4c0-21-7.9-41.1-15.7-55.9-1.7-3.1-4.5-1.7-4.5 3.9z" fill="#fff"/>
-</svg>
-        """
+        """Return SVG icon content, preferring a local icon.svg in the plugin folder."""
+        icon_path = os.path.join(os.path.dirname(__file__), 'icon.svg')
+        try:
+            if os.path.exists(icon_path):
+                with open(icon_path, 'r', encoding='utf-8') as f:
+                    return f.read()
+        except Exception:
+            pass
+        return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10" fill="#ff6600"/><path fill="#fff" d="M11 7h2v10h-2z"/></svg>'
 
     def get_connection_data(self):
         """
